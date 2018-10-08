fbc1ccc4b0f5e74e15ecc3f0051179d69ebafebc
sentrip <djordje.m.pepic@gmail.com>
2018-10-09 01:35:11 +0200

    Fixed untracked files

---
+++ anybot/actions/__init__.py
@@ -0,0 +1,9 @@
+from .instagram import (
+    PeriodicLoginOut, FollowNewUsers, KeepUnderFollowLimit,
+    LikeLeastLikedMedia, UnfollowAfter, UnfollowNotFollowing
+)
+
+__all__ = [
+    'PeriodicLoginOut', 'FollowNewUsers', 'KeepUnderFollowLimit',
+    'LikeLeastLikedMedia', 'UnfollowAfter', 'UnfollowNotFollowing',
+]
